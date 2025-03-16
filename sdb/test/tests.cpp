9172c17839fd6b4c9b8100c9b97bdc272b143799
xingjiaxin <jiaxin.cuckoo@gmail.com>
2025-03-16 15:11:00 +0800

    0316 - Manual Testing.

--- sdb/test/tests.cpp
+++ sdb/test/tests.cpp
@@ -1,9 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("validate environment") {
+TEST_CASE("validate environment")
+{
 	REQUIRE(true);
 }
 
-TEST_CASE("validate environment false") {
+TEST_CASE("validate environment false")
+{
 	REQUIRE(false);
 }
